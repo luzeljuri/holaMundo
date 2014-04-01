b5c180ac30e7c184c863169dbdcceb5678681a84
Luz <lucesita472@gmail.com>
2014-04-01 08:23:29 -0430

    1er commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HolaMundo::Application.config.secret_key_base = '1b9d40087372f801f4e775192fc6a74d070cde0c4ff949caa9a64183e965d90494ed86dabad48442a1cfe030fc6cce8062cb6bce527aa19e3df3330bb518e463'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HolaMundo::Application.config.session_store :cookie_store, key: '_holaMundo_session'
